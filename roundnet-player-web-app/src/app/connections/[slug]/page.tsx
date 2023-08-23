4cdc7f7aa2d2a1c6b1f9f891aa0ccbacdf7c3c6c
Cory Chilton <coryhookchilton@gmail.com>
2023-08-22 21:43:33 -0700

    connection page able to fetch connection data

--- roundnet-player-web-app/src/app/connections/[slug]/page.tsx
+++ roundnet-player-web-app/src/app/connections/[slug]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useEffect } from "react";
+import { useSearchParams } from 'next/navigation';
 
 interface pageProps{
   params: {slug: string}
@@ -7,12 +8,17 @@ interface pageProps{
 
 export default function ConnectionPage({params}: pageProps) {
   const [connection, setConnection] = useState(null);
-  // useEffect(() => {
-  //   fetchConnection(`http://localhost:80/api/graph/both?${params.slug}`).then(data => setConnection(data));
-  // }, []);
-  // console.log(connection);
 
-  return <div>{params.slug}</div>
+  const searchParams = useSearchParams();
+  const source = searchParams.get('source');
+  const destination = searchParams.get('destination');
+
+  useEffect(() => {
+    fetchConnection(`http://localhost:80/api/graph/both?source=${source}&destination=${destination}`).then(data => setConnection(data));
+  }, []);
+  console.log(connection[0]);
+
+  return <div>{JSON.stringify(connection)}</div>
 }
 
 async function fetchConnection(url:string) {
