0f67ab40f7846fd4d444d9372195934dce83a89a
Cory Chilton <coryhookchilton@gmail.com>
2023-08-26 20:28:57 -0700

    fixed ConnectionPlayerSelect mobile bug

--- src/components/connections/ConnectionPlayerSelect.tsx
+++ src/components/connections/ConnectionPlayerSelect.tsx
@@ -28,10 +28,10 @@ export default function ConnectionPlayerSelect({
   searchedPlayers.sort();
 
   return (
-    <div className="flex flex-col items-center relative w-96">
+    <div className="flex flex-col items-center relative w-full">
       <h3 className="text-lg font-extralight text-gray-300 mb-2">{label}</h3>
       <div>
-        <input value={selectedPlayer} onChange={playerClick} list={listId} placeholder="Select a player" className="focus:bg-gray-900 bg-inherit text-3xl border border-gray-500 drop-shadow-glow ease-in-out duration-500 rounded-full px-4 py-2 text-shadow-glow shadow-purple-600 text-gray-300 font-semibold placeholder:text-gray-500 placeholder:font-thin placeholder:text-shadow-none focus:outline-none" />
+        <input value={selectedPlayer} onChange={playerClick} list={listId} placeholder="Select a player" className="w-full focus:bg-gray-900 bg-inherit text-3xl border border-gray-500 drop-shadow-glow ease-in-out duration-500 rounded-full px-4 py-2 text-shadow-glow shadow-purple-600 text-gray-300 font-semibold placeholder:text-gray-500 placeholder:font-thin placeholder:text-shadow-none focus:outline-none" />
         <datalist id={listId} >
           {searchedPlayers.map(player => 
             <option key={player} value={firstLetterUpper(player)} />  
