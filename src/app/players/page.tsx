2bbde4e4bac8b7695383c51f8f3310864f545cc3
Cory Chilton <coryhookchilton@gmail.com>
2023-08-27 10:44:54 -0700

    changed featured game to using grid instead of flexbox

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -12,7 +12,7 @@ export default function PlayersPage(){
         Players
       </h1>
       <p className="max-w-2xl text-center text-gray-300 mb-4">
-        Find statistics on any roundnet player that has participatedn in an STS tournament so far in the 2023 season.
+        Find statistics on any roundnet player that has participated in an STS tournament so far in the 2023 season.
       </p>
       <PlayerSelect />
     </div>
--- src/components/player/FeaturedGame.tsx
+++ src/components/player/FeaturedGame.tsx
@@ -18,39 +18,36 @@ export default function FeaturedGame({
   const players2 = JSON.parse(game.players_2);
 
   return (
-    <div className="flex flex-col w-full sm:w-[40rem] items-center border-gray-200 shadow-glow shadow-pink-300 p-2">
-      <h2 className="text-2xl text-gray-200 text-shadow-glow shadow-yellow-700 mb-2">{title}</h2>
-      <div className="flex items-start justify-center w-full">
-        <ScoreTeamPlayers score={game.score_1} team={game.team_1} players={players1} />
-        <div className="flex flex-col items-center">
-          <div className="text-lg text-gray-500">vs</div>
-          <div className="text-gray-500 font-bold text-3xl mt-5">-</div>
-        </div>
-        <ScoreTeamPlayers score={game.score_2} team={game.team_2} players={players2} />
-      </div>
-      <h3 className="text-gray-300 mt-3">
+    <div className="grid grid-cols-[1fr_0.1fr_1fr] border-gray-200 shadow-glow shadow-pink-300 w-full max-w-2xl p-2">
+      <h2 className="text-2xl text-gray-200 text-shadow-glow shadow-yellow-700 col-span-3 text-center mb-1">{title}</h2>
+      <h4 className="text-lg text-gray-300 font-light text-center break-words w-0 min-w-full px-2 flex flex-col justify-end">
+        {game.team_1}
+      </h4>
+      <div className="text-lg text-gray-500 text-center flex flex-col justify-end">vs</div>
+      <h4 className="text-lg text-gray-300 font-light text-center break-words w-0 min-w-full px-2 flex flex-col justify-end">
+        {game.team_2}
+      </h4>
+      <h3 className="text-5xl text-pink-200 text-shadow-glow shadow-pink-500 font-semibold text-center">
+        {game.score_1}
+      </h3>
+      <div className="text-gray-500 font-bold text-3xl text-center">-</div>
+      <h3 className="text-5xl text-pink-200 text-shadow-glow shadow-pink-500 font-semibold text-center">
+        {game.score_2}
+      </h3>
+      <h4 className="text-gray-300 font-extralight text-center mt-1">
+        {firstLetterUpper(players1[0])} and {firstLetterUpper(players1[1])}
+      </h4>
+      <h4 className="text-gray-300 font-extralight text-center col-start-3 mt-1">
+        {firstLetterUpper(players2[0])} and {firstLetterUpper(players2[1])}
+      </h4>
+      <h3 className="text-gray-300 col-span-3 text-center mt-2">
         {tournamentDict.get(game.tournament_name)} ({game.division})
       </h3>
-      <h4 className="text-gray-300 font-light mt-1">
+      <h4 className="text-gray-300 font-light text-center col-span-3">
         {firstLetterUpper(game.tournament_stage)}
       </h4>
     </div>
   )
 }
 
-function ScoreTeamPlayers({score, team, players}:{score:number, team:string, players:string[]}) {
-  return (
-    <div className="flex flex-col items-center w-1/2 px-1">
-      <h4 className="text-lg text-gray-300 font-light mb-4 text-center w-full break-words balance">
-        {team}
-      </h4>
-      <h3 className="text-5xl text-pink-200 text-shadow-glow shadow-pink-500 font-semibold mb-4">
-        {score}
-      </h3>
-      <h4 className="text-gray-300 font-extralight text-center">
-        {firstLetterUpper(players[0])} and {firstLetterUpper(players[1])}
-      </h4>
-    </div>
-  )
-}
 
