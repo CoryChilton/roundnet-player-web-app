d80c7eca4da977c61047361dc4905b264633d9e8
Cory Chilton <coryhookchilton@gmail.com>
2023-08-27 13:03:53 -0700

    added loading player names and no player with name x to connection ConnectionPlayerSelect

--- src/components/connections/ConnectionPlayerSelect.tsx
+++ src/components/connections/ConnectionPlayerSelect.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getAllPlayers, searchArray, firstLetterUpper } from "@/utilities/utils";
 import Loading from "../general/Loading";
-import { list } from "postcss";
 
 export default function ConnectionPlayerSelect({
   selectedPlayer, 
@@ -46,22 +45,19 @@ export default function ConnectionPlayerSelect({
         <h2 className="text-3xl text-shadow-glow shadow-purple-600 text-gray-300 font-semibold px-4 py-2 text-center">{selectedPlayer}</h2>
       }
       <div className={`absolute bg-gray-900 top-full left-0 pt-2 w-full z-20 rounded-xl border-gray-800 border overflow-auto max-h-96 ${!searchInput && 'hidden'}`}>
+        {!players.length ?
+          <h2 className="text-left pl-3 py-1 text-yellow-500 text-lg">Getting all player names...</h2>
+        : !searchedPlayers.length ?
+          <h2 className="text-center text-red-400 text-lg px-2">
+            NO PLAYERS FOUND WITH THE NAME: {'"'}{searchInput.toUpperCase()}{'"'}
+          </h2>
+        : null
+        }
+        
         {searchedPlayers.map(player =>
           <button key={player} onClick={playerOptionSelect} className="block w-full text-left pl-3 py-1 text-gray-300 text-lg hover:bg-gray-700">{firstLetterUpper(player)}</button>
         )}
-
       </div>
     </div>
-    // <div className="flex flex-col items-center relative w-full">
-    //   <h3 className="text-lg font-extralight text-gray-300 mb-2">{label}</h3>
-    //   <div>
-    //     <input value={selectedPlayer} onChange={playerClick} list={listId} placeholder="Select a player" className="w-full focus:bg-gray-900 bg-inherit text-3xl border border-gray-500 drop-shadow-glow ease-in-out duration-500 rounded-full px-4 py-2 text-shadow-glow shadow-purple-600 text-gray-300 font-semibold placeholder:text-gray-500 placeholder:font-thin placeholder:text-shadow-none focus:outline-none" />
-    //     <datalist id={listId} >
-    //       {searchedPlayers.map(player => 
-    //         <option key={player} value={firstLetterUpper(player)} />  
-    //       )}
-    //     </datalist>
-    //   </div>
-    // </div>
   )
 }
